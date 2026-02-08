627a29bb18dc206735e098d56b34366b8dbfbb3f
Solomon Tsehay <solomontsehay54@gmail.com>
2026-02-08 20:19:10 +0300

    Update next.config.ts

--- nexus-movie/next.config.ts
+++ nexus-movie/next.config.ts
@@ -1,8 +1,43 @@
+// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "image.tmdb.org",
+        port: "",
+        pathname: "/t/p/**",
+      },
+      {
+        protocol: "https",
+        hostname: "via.placeholder.com",
+      },
+      {
+        protocol: "https",
+        hostname: "taqrgsafgexysnqrvzje.supabase.co",
+        port: "",
+        pathname: "/storage/v1/object/public/**",
+      },
+
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        port: "",
+        pathname: "/**",
+      },
+
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
